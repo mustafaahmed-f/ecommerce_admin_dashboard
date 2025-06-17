f96211031d6e2201dd2aeda299843897231a0136
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-06-17 22:13:42 +0300

    ✨ feat(fakedata): added products fake data and used it in the tanstack table instance

--- app/_features/products/table/table.tsx
+++ app/_features/products/table/table.tsx
@@ -2,13 +2,14 @@ import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { generalColumns } from "./columns";
 import { Product } from "../types/productType";
 import { useMemo } from "react";
+import { products } from "../utils/fakeData";
 
 export function useDataTable() {
   const columns = useMemo(() => generalColumns(true, "products"), []);
 
   const table = useReactTable<Product>({
     columns,
-    data: [],
+    data: products,
     getCoreRowModel: getCoreRowModel(),
   });
 
